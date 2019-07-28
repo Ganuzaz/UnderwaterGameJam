eb85b681b90e881763543cf2efff542d9073645b
Ganuzaz <Ganuzaz@Gmail.com>
2019-07-28 21:12:01 +0700

    asd

--- UnderwaterGameJam/Assets/Scripts/BridgeScript.cs
+++ UnderwaterGameJam/Assets/Scripts/BridgeScript.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class BridgeScript : MonoBehaviour
 {
     
-    private List<GameObject> ThingsAboveBridges;
-    private List<ContactPoint2D> ContactPoint2Ds;
+    private List<Enemy> onColliderList;
+
     // Start is called before the first frame update
     void Start()
     {
-        ThingsAboveBridges = new List<GameObject>();
-        ContactPoint2Ds = new List<ContactPoint2D>();
+        
     }
 
     // Update is called once per frame
@@ -21,8 +20,28 @@ public class BridgeScript : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
+    {        
+        if (collision.transform.GetComponent<Enemy>()!=null)
+        {
+            onColliderList.Add(collision.transform.GetComponent<Enemy>());
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        collision.GetContacts(ContactPoint2Ds);
-        Debug.Log(ContactPoint2Ds[0]);
-    }    
+        if(collision.transform.GetComponent<Enemy>()!=null)
+            if (onColliderList.Contains(collision.transform.GetComponent<Enemy>()))
+            {
+                onColliderList.Remove(collision.transform.GetComponent<Enemy>());
+            }
+    }
+
+    public void DamageAll()
+    {
+        foreach(var element in onColliderList)
+        {
+            element.Damage();
+        }
+    }
+
 }
